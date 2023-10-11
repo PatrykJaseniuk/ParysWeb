5be3a3ba14602de5aaf09ce5f8779a760ab42f79
Patryk Jaseniuk <patryk.jaseniuk@gmail.com>
2023-10-11 23:16:28 +0200

    poprawa sekcji kontakt

--- components/Sekcje/Kontakt.tsx
+++ components/Sekcje/Kontakt.tsx
@@ -13,20 +13,20 @@ export const Kontakt = ({ data }: { data: KontaktDane[] }) => {
     const theme = useMantineTheme()
     return (
         <Box bg={"blue"} >
-            <Container p={'xl'} size={'xl'} >
+            <Container size={'xl'} p={0} >
                 <SimpleGrid cols={{ base: 1, sm: 2 }} spacing={50}>
-                    <div>
+                    <Container>
                         <Title order={3}>Kontakt</Title>
                         <ContactIconsList data={data} />
                         <Group mt="xl">{icons}</Group>
-                    </div>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2538.8985260142413!2d17.32172837733394!3d50.4802327852178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4711c4125f989f0b%3A0xb3c7ba6aef95e9fb!2sSI%C5%81OWNIA%20Parys%20Nysa!5e0!3m2!1spl!2spl!4v1689690588688!5m2!1spl!2spl" style={{ width: '100%', height: '40rem' }} loading="lazy" ></iframe>
+                    </Container>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2538.8985260142413!2d17.32172837733394!3d50.4802327852178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4711c4125f989f0b%3A0xb3c7ba6aef95e9fb!2sSI%C5%81OWNIA%20Parys%20Nysa!5e0!3m2!1spl!2spl!4v1689690588688!5m2!1spl!2spl" style={{ width: '100%', height: '40rem' }} loading="lazy" >
+                    </iframe>
                 </SimpleGrid>
-                <Divider c={'red'} size={'md'} m={'xl'} />
-                <Group __size={'sm'} align="end" justify="end" mt="xl">
+                <Divider c={'red'} size={'md'} mt={'md'} />
+                <Group __size={'sm'} align="end" justify="end" p="md">
                     <Text >Written by Patryk</Text>
                     <IconGithub /></Group>
-                <Space h={'xl'} />
 
             </Container>
 
