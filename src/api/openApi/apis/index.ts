8a14e212dd1189ffdae8a6068a6802237394adb9
Patryk Jaseniuk <patryk.jaseniuk@gmail.com>
2025-02-04 19:13:46 +0100

    zmiany

---
+++ src/api/openApi/apis/index.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ArticleApi';
+export * from './GlobalApi';
+export * from './HeaderApi';
+export * from './KarnetyApi';
+export * from './KontaktApi';
+export * from './PromocjaApi';
+export * from './StrefaApi';
+export * from './TrenerApi';
+export * from './UploadFileApi';
+export * from './UsersPermissionsAuthApi';
+export * from './UsersPermissionsUsersRolesApi';
+export * from './UslugaSilowniaApi';
+export * from './UslugiApi';
+export * from './ZajeciaGrupoweApi';
+export * from './ZajeciaIndywidualneApi';
+export * from './ZajecieGrupoweApi';
